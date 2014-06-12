c010f6e97b6e9818f476a66624f70f5e809e3963
Massimo Gambineri <mgambineri@gmail.com>
2014-06-12 20:23:10 +0200

    Made CameraPreview inner class of CameraActivity as they are intrinsically tide each other

--- src/main/java/com/imdp/instaclimb/CameraActivity.java
+++ src/main/java/com/imdp/instaclimb/CameraActivity.java
@@ -14,6 +14,8 @@ import android.hardware.Camera.PictureCallback;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -30,11 +32,6 @@ import java.util.ListIterator;
 public class CameraActivity extends Activity {
 
   private Camera        m_Camera      = null;
-
-  public int getM_CameraId() {
-    return m_CameraId;
-  }
-
   private int           m_CameraId    = -1;
   private CameraPreview m_Preview     = null;
   private SessionImage  m_SessionImg  = null;
@@ -66,9 +63,6 @@ public class CameraActivity extends Activity {
 
       //Create image wrapper obj for this session
       m_SessionImg = new SessionImage(getResources().getString(R.string.app_name));
-
-      // Set the camera in CameraPreview view
-      m_Preview.setCamera(m_Camera);
     }
   }
 
@@ -267,4 +261,95 @@ public class CameraActivity extends Activity {
 
     return ret;
   }
-} // class CameraActivity
+
+  /**
+   * The Camera Preview class
+   * */
+  public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+    private   SurfaceHolder m_Holder = null;
+    protected Activity      m_Activity = null;
+
+    public CameraPreview(Context context) {
+      super(context);
+
+      m_Activity = (Activity)context;
+
+      // Install a SurfaceHolder.Callback so we get notified when the
+      // underlying surface is created and destroyed.
+      m_Holder = getHolder();
+      if (m_Holder != null) {
+        m_Holder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        m_Holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+      } else throw new AssertionError("m_Holder is null in CameraPreview(Context context, Camera camera)");
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+      // The Surface has been created, now tell the camera where to draw the preview.
+      try {
+        m_Camera.setPreviewDisplay(holder);
+        m_Camera.startPreview();
+      } catch (IOException e) {
+        Log.d(Helpers.Const.DBGTAG, "Error setting camera preview: " + e.getMessage());
+      }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+      // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+      // If your preview can change or rotate, take care of those events here.
+      // Make sure to stop the preview before resizing or reformatting it.
+      if (m_Holder.getSurface() == null)
+        return; // preview surface does not exist
+
+      if (m_Camera == null)
+        return;
+
+      // stop preview before making changes
+      try {
+        m_Camera.stopPreview();
+      } catch (Exception e){
+        // ignore: tried to stop a non-existent preview
+      }
+
+      // set preview size and make any resize, rotate or reformatting changes here
+      // and start preview with new settings
+      try {
+        m_Camera.setDisplayOrientation(
+            Helpers.Do.getRotationRelativeToNaturalOrientaton(m_Activity, m_CameraId, m_Camera));
+        m_Camera.setPreviewDisplay(m_Holder);
+        m_Camera.startPreview();
+      } catch (Exception e){
+        Log.d(Helpers.Const.DBGTAG, "Error starting camera preview: " + e.getMessage());
+      }
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+      int parentWidth = View.MeasureSpec.getSize(widthMeasureSpec);
+      int parentHeight = View.MeasureSpec.getSize(heightMeasureSpec);
+
+//    int top = (parentHeight-parentWidth)/2;
+//    FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(parentWidth, parentWidth);
+//    layoutParams.setMargins(0, top, parentWidth, parentWidth);
+//    setLayoutParams(layoutParams);
+
+      setMeasuredDimension(parentWidth, parentHeight);
+
+//    View bf = findViewById(R.id.bottom_frame);
+//    if (bf!=null) {
+//      Helpers.Do.msgBox(m_Activity, parentHeight  + "-" + bf.getHeight());
+//      bf.setTop(parentHeight - bf.getHeight());
+//    }
+
+//    Helpers.Do.msgBox(m_Activity, this.getWidth() + "");
+//    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+  } // *** class CameraPreview
+
+} // *** class CameraActivity

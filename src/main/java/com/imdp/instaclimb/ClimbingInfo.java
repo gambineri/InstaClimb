775c467d844ba6b6cc077c312499164ff683d34f
gambineri <mgambineri@gmail.com>
2014-12-31 18:50:45 +0100

    added climbing info activity - will replace modal dialog for user input on startup

--- src/main/java/com/imdp/instaclimb/ClimbingInfo.java
+++ src/main/java/com/imdp/instaclimb/ClimbingInfo.java
@@ -1,20 +1,25 @@
 package com.imdp.instaclimb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
 
 
 public class ClimbingInfo extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.climbing_info);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-    }
+//    getWindow().setWindowAnimations(android.R.anim.slide_in_left);
+//    getWindow().setWindowAnimations(android.R.anim.slide_out_right);
+    setContentView(R.layout.climbing_info);
+    PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+  }
 
+  public void onNext(View v) {
+    ClimbingInfo.this.startActivity(new Intent(ClimbingInfo.this, CameraActivity.class));
+  }
 }

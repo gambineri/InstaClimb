fac1845c42ae4d8693fdb62ff0090a0d7b48198d
gambineri <mgambineri@gmail.com>
2015-01-02 23:19:24 +0100

    refined autocomplete functionality

--- src/main/java/com/imdp/instaclimb/ClimbingInfo.java
+++ src/main/java/com/imdp/instaclimb/ClimbingInfo.java
@@ -1,19 +1,16 @@
 package com.imdp.instaclimb;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.*;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 
-
 public class ClimbingInfo extends Activity {
-  AutoCompleteTextView m_AscNameCtl = null;
+  AutoCompleteTextView m_AscNameCtl  = null;
+  AutoCompleteTextView m_LocationCtl = null;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +20,11 @@ public class ClimbingInfo extends Activity {
     PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
     // Get a reference to the AutoCompleteTextView in the layout
-    m_AscNameCtl = (AutoCompleteTextView) findViewById(R.id.ascentname);
+    m_AscNameCtl  = (AutoCompleteTextView) findViewById(R.id.ascentname);
+    m_LocationCtl = (AutoCompleteTextView) findViewById(R.id.location);
 
-    loadPrefData(m_AscNameCtl, "IC_ASCENTNAME_HISTORY");
+    Helpers.Do.loadPrefData(this, m_AscNameCtl, Helpers.Const.ASCENTNAME_HIST);
+    Helpers.Do.loadPrefData(this, m_LocationCtl, Helpers.Const.LOCATION_HIST);
   }
 
   public void onNext(View v) {
@@ -39,46 +38,9 @@ public class ClimbingInfo extends Activity {
     i.putExtra(Helpers.Const.EXTRA_ASCENT_NAME, ascentname);
     i.putExtra(Helpers.Const.EXTRA_LOCATION, location);
 
-    savePrefData(ascentname, m_AscNameCtl, "IC_ASCENTNAME_HISTORY");
+    Helpers.Do.savePrefData(this, m_AscNameCtl, Helpers.Const.ASCENTNAME_HIST, ascentname);
+    Helpers.Do.savePrefData(this, m_LocationCtl, Helpers.Const.LOCATION_HIST, location);
 
     ClimbingInfo.this.startActivity(i);
-  }
-
-  private void loadPrefData(AutoCompleteTextView autoComplete, String shared_pref_name) {
-    SharedPreferences settings = getSharedPreferences(shared_pref_name, Context.MODE_PRIVATE);
-    int size = settings.getInt("size", 0);		// Getting the number of existing lines
-    if (size != 0) { 					                // size is 0 if no lines was saved
-      String[] history = new String[size];		// Create array of strings for the lines
-      for (int i = 0; i<size; i++) {		      //Save the lines to the array of strings
-        history[i] = settings.getString(String.valueOf(i+1), "empty");
-      }
-      //Updating the adapter
-      ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, history);
-      autoComplete.setAdapter(adapter);
-//      autoComplete.setText(saved_uri); //loading saved url
-    }
-  }
-
-  private void savePrefData(String newStr, AutoCompleteTextView autoComplete, String shared_pref_name) {
-    SharedPreferences settings = getSharedPreferences(shared_pref_name, Context.MODE_PRIVATE);
-    int size = settings.getInt("size", 0);
-    String[] history = new String[size+1];		  // Create array with one more place for the new string
-    boolean repeat = false;
-    for (int i=0; i<size; i++) {                // Checking if the new line does not already exist
-      history[i] = settings.getString(String.valueOf(i+1), "empty");
-      if (history[i].equals(newStr)) {
-        repeat = true;
-        break;
-      }
-    }
-    if (!repeat) {
-      SharedPreferences.Editor editor = settings.edit();  // Saving the new string
-      editor.putString(String.valueOf(size+1), newStr);
-      editor.putInt("size", size+1);
-      editor.apply();
-      history[size] = newStr; //Updating the adapter online
-      ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, history);
-      autoComplete.setAdapter(adapter);
-    }
   }
 }
--- src/main/java/com/imdp/instaclimb/Helpers.java
+++ src/main/java/com/imdp/instaclimb/Helpers.java
@@ -2,12 +2,16 @@ package com.imdp.instaclimb;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.hardware.Camera;
 import android.view.Surface;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 public final class Helpers {
-  
+  /* CONST */
 	public final static class Const {
     public static final String DBGTAG = "IMDP";
 		public static final String CAPTURED_IMG_PREFIX = "CAPTUREDIMG_";
@@ -17,26 +21,12 @@ public final class Helpers {
     public static final String EXTRA_TOP_FRAME_W        = "topFrameWidth";
     public static final String EXTRA_ASCENT_NAME        = "ascentName";
     public static final String EXTRA_LOCATION           = "location";
+    public static final String ASCENTNAME_HIST          = "IC_ASCENTNAME_HISTORY";
+    public static final String LOCATION_HIST            = "IC_LOCATION_HISTORY";
   }
 
-  public static String toCamelCase(String input, String separator, String replacement) {
-    if (input == null || input.length() <= 0)
-      return "";
-
-    if (replacement == null)
-      replacement = separator;
-
-    String[] words = input.toLowerCase().split(java.util.regex.Pattern.quote(separator));
-    StringBuilder ret = new StringBuilder();
-
-    for (String word : words)
-      if (word != null && word.length() > 0)
-        ret.append(word.substring(0, 1).toUpperCase()).append(word.substring(1)).append(replacement);
-
-    return ret.toString().substring(0, ret.toString().length() - replacement.length());
-  }
-
-	public final static class Do {
+  /* METHODS */
+  public final static class Do {
 		public static void msgBox(Activity activity, String msg) {
 			// 1. Instantiate an AlertDialog.Builder with its constructor
 			AlertDialog.Builder builder = new AlertDialog.Builder(activity);
@@ -47,11 +37,28 @@ public final class Helpers {
 			       .setPositiveButton("Ok", null);
 
 			// 3. Get the AlertDialog from create()
-			AlertDialog dialog = builder.create();		
-			
-			// 4. Display the dialog 
+			AlertDialog dialog = builder.create();
+
+			// 4. Display the dialog
 			dialog.show();
 		}
+
+    public static String toCamelCase(String input, String separator, String replacement) {
+      if (input == null || input.length() <= 0)
+        return "";
+
+      if (replacement == null)
+        replacement = separator;
+
+      String[] words = input.toLowerCase().split(java.util.regex.Pattern.quote(separator));
+      StringBuilder ret = new StringBuilder();
+
+      for (String word : words)
+        if (word != null && word.length() > 0)
+          ret.append(word.substring(0, 1).toUpperCase()).append(word.substring(1)).append(replacement);
+
+      return ret.toString().substring(0, ret.toString().length() - replacement.length());
+    }
 
     public static void toast(Activity activity, String msg) {
       Toast.makeText(activity, msg, Toast.LENGTH_LONG).show();
@@ -81,6 +88,47 @@ public final class Helpers {
         result = (info.orientation - degrees + 360) % 360;
       }
       return result;
+    }
+
+    public static void loadPrefData(Context ctx, AutoCompleteTextView autoComplete, String shared_pref_name) {
+      SharedPreferences prefs = ctx.getSharedPreferences(shared_pref_name, Context.MODE_PRIVATE);
+      int               size  = prefs.getInt("size", 0);		// Getting the number of existing lines
+
+      if (size != 0) { 					                // size is 0 if no lines was saved
+        String[] history = new String[size];		// Create array of strings for the lines
+        for (int i = 0; i<size; i++) {		      //Save the lines to the array of strings
+          history[i] = prefs.getString(String.valueOf(i+1), "empty");
+        }
+
+        //Updating the adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ctx, android.R.layout.simple_dropdown_item_1line, history);
+        autoComplete.setAdapter(adapter);
+      }
+    }
+
+    public static void savePrefData(Context ctx, AutoCompleteTextView autoComplete, String shared_pref_name, String newStr) {
+      SharedPreferences prefs   = ctx.getSharedPreferences(shared_pref_name, Context.MODE_PRIVATE);
+      int               size    = prefs.getInt("size", 0);
+      String[]          history = new String[size+1];		  // Create array with one more place for the new string
+      boolean           repeat  = false;
+
+      for (int i=0; i<size; i++) {                // Checking if the new line does not already exist
+        history[i] = prefs.getString(String.valueOf(i+1), "empty");
+        if (history[i].equals(newStr)) {
+          repeat = true;
+          break;
+        }
+      }
+
+      if (!repeat) {
+        SharedPreferences.Editor editor = prefs.edit();  // Saving the new string
+        editor.putString(String.valueOf(size+1), newStr);
+        editor.putInt("size", size+1);
+        editor.apply();
+        history[size] = newStr; //Updating the adapter online
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ctx, android.R.layout.simple_dropdown_item_1line, history);
+        autoComplete.setAdapter(adapter);
+      }
     }
   }
 }

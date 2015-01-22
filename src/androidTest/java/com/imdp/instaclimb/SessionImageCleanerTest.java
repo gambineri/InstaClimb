dde6fa08f38db89aee222d3a88c0994b99094504
gambineri <mgambineri@gmail.com>
2015-01-22 21:58:29 +0100

    added test for session image cleaner

--- src/androidTest/java/com/imdp/instaclimb/SessionImageCleanerTest.java
+++ src/androidTest/java/com/imdp/instaclimb/SessionImageCleanerTest.java
@@ -1,31 +1,54 @@
 package com.imdp.instaclimb;
 
+import android.util.Log;
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.util.Calendar;
 
 public class SessionImageCleanerTest extends TestCase {
+  private final static int NUM_TEST_FILES = 2;
+  File m_img_folder = null;
+  File[] files_to_del = new File[10];
 
   @Before
   public void setUp() throws Exception {
+//    SessionImage si = new SessionImage(getResources().getString(R.string.app_name));
+    SessionImage si = new SessionImage("instaclimb");
+    m_img_folder = si.getCapturedImageDir();
 
+    for (int i=0; i<NUM_TEST_FILES; i++) {
+      Log.d(Helpers.Const.DBGTAG, m_img_folder + "/file" + i + ".png");
+      files_to_del[i] = new File(m_img_folder + "/file" + i + ".png");
+      files_to_del[i].createNewFile();
+    }
+
+    Calendar time = Calendar.getInstance();
+    time.add(Calendar.DAY_OF_YEAR, -31);
+
+    boolean ret = false;
+    long t = 0;
+    t = (time.getTimeInMillis()/1000)*1000;
+    for (int i=0; i<NUM_TEST_FILES/2; i++) {
+      Log.d(Helpers.Const.DBGTAG, "t = " + t);
+      ret = files_to_del[i].setLastModified(t);
+      Log.d(Helpers.Const.DBGTAG, "" + Boolean.toString(ret));
+    }
   }
 
   @After
   public void tearDown() throws Exception {
-
+    for (int i=0; i<NUM_TEST_FILES; i++) {
+//      files_to_del[i].delete();
+    }
   }
 
   @Test
   public void testDoInBackground() throws Exception {
-
-  }
-
-  @Test
-  public void testDoInBackground1() throws Exception {
-    assertEquals(false, 1==2);
+    SessionImageCleaner sic = new SessionImageCleaner(m_img_folder, 0);
+    sic.execute(sic.m_Params);
   }
 }
--- src/main/java/com/imdp/instaclimb/SessionImageCleaner.java
+++ src/main/java/com/imdp/instaclimb/SessionImageCleaner.java
@@ -1,6 +1,7 @@
 package com.imdp.instaclimb;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.File;
 import java.util.*;
@@ -8,42 +9,42 @@ import java.util.*;
 /**
  * Created by massimo on 08/01/15.
  */
-public class SessionImageCleaner extends AsyncTask<File, Void, Void> {
+public class SessionImageCleaner extends AsyncTask<SessionImageCleaner.Params, Void, Void> {
 
-  /* Num of days images are retained. Could be a user setting in the future */
-  private final static int MAX_RETENTION_DAYS = 30;
   /* Num of milliseconds in 24 hours */
   private final static int MILLIS_PER_DAY = 86400000;
 
+  class Params {
+    int  m_RetentionDays = -1;
+    File m_Folder        = null;
+  }
+
+  /* Params for this cleaner instance */
+  Params m_Params = new Params();
+
+  public SessionImageCleaner(File folder_path, int retention_days) {
+    m_Params.m_RetentionDays = retention_days;
+    m_Params.m_Folder = folder_path;
+  }
+
   @Override
-  protected Void doInBackground(File...img_folder) {
+  protected Void doInBackground(SessionImageCleaner.Params...params) {
 
-//    final File[] sortedByDate = img_folder[0].listFiles();
-//
-//    if (sortedByDate != null && sortedByDate.length > 1) {
-//      Arrays.sort(sortedByDate, new Comparator<File>() {
-//        @Override
-//        public int compare(File object1, File object2) {
-//          return (int) ((object1.lastModified() > object2.lastModified()) ? object1.lastModified() : object2.lastModified());
-//        }
-//      });
-//    }
-//
-//    final long max_retention_millis = MAX_RETENTION_DAYS*MILLIS_PER_DAY;
-//    final long cut_time = GregorianCalendar.getInstance().getTimeInMillis();
-
+    if (m_Params == null) {
+      Log.d(Helpers.Const.DBGTAG, "Could not create SessionImageCleaner: m_Params is null.");
+      return null;
+    }
 
     Calendar time = Calendar.getInstance();
-    time.add(Calendar.DAY_OF_YEAR, -7);
+    time.add(Calendar.DAY_OF_YEAR, -1*params[0].m_RetentionDays);
 
-    File[] files = img_folder[0].listFiles();
+    File[] files = params[0].m_Folder.listFiles();
     for (File f : files) {
       if (f.isFile()) {
         Date lastModified = new Date(f.lastModified());
         if(lastModified.before(time.getTime())) {
-          //file is older than a week
+          f.delete(); //file is older than XX days
         }
-
       }
     }
 
